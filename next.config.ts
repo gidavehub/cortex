9949ea7c7e0d0c8fa2cabc99f4f9c6e841f94493
Godswill Iyke Dave <godswilldave1@gmail.com>
2026-02-02 11:18:35 +0000

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+// @ts-ignore - eslint and typescript property might not be in the strict type depending on version
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
